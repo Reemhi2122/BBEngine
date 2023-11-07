07b54c333b62f8ab603d34cb05745ad434bc1bac
reemhi2122 <stanvogels@live.nl>
2023-11-07 22:31:26 +0000

    Added asset files and created copy commands in cmake

--- BBEngine/UnitTest/DataLoadingTest.cpp
+++ BBEngine/UnitTest/DataLoadingTest.cpp
@@ -5,7 +5,7 @@
 TEST(Fileloading, WAV)
 {
     BBE::WAV WAVFile;
-    WAVFile.LoadWav("assets/Sound/music.wav");
+    WAVFile.LoadWav("Assets/Sound/music.wav");
     WAVFile.WriteWav("music_copy.wav");
 }
 
@@ -17,7 +17,7 @@ TEST(Fileloading, Bitmap)
 TEST(Fileloading, Json)
 {
     BBE::JsonParser parser;
-    parser.Parse("assets/Data/sample.json");
+    parser.Parse("Assets/Data/sample.json");
     parser.GetRootNode()["children"]->GetListBB()[4]->GetObjectBB()["name"]->SetStringBB("Stan Vogels");
     parser.GetRootNode()["children"]->GetListBB()[4]->GetObjectBB()["age"]->SetFloatBB(29);
     parser.WriteJson("writebackJson.json");
