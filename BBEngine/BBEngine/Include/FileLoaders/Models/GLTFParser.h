6106b8140c9c2be62b436b6aeb80194b7a00c8ef
Reemhi2122 <stanvogels@live.nl>
2023-12-13 17:17:04 +0000

    Added indices loading to GLTFFile

--- BBEngine/BBEngine/Include/FileLoaders/Models/GLTFParser.h
+++ BBEngine/BBEngine/Include/FileLoaders/Models/GLTFParser.h
@@ -6,15 +6,16 @@ namespace BBE {
 	struct GLTFFile
 	{
 		Vector3* Vertices;
+		uint32_t* Indices;
 	};
 
 	class GLTFParser {
 	public:
 		GLTFParser();
-		GLTFParser(std::string m_GLTFPath, std::string m_BinFile);
+		GLTFParser(std::string m_GLTFPath);
 		~GLTFParser();
 
-		GLTFFile* Parse(std::string m_GLTFPath, std::string m_BinFile);
+		GLTFFile* Parse(std::string m_GLTFPath);
 	
 	private:
 		BBSystem::BBFILE m_BinFile;
--- BBEngine/BBEngine/Source/FileLoaders/Models/GLTFParser.cpp
+++ BBEngine/BBEngine/Source/FileLoaders/Models/GLTFParser.cpp
@@ -5,8 +5,8 @@ namespace BBE {
 
 	GLTFParser::GLTFParser() {}
 
-	GLTFParser::GLTFParser(std::string a_GLTFPath, std::string a_BinFile) {
-		Parse(a_GLTFPath, a_BinFile);
+	GLTFParser::GLTFParser(std::string a_GLTFPath) {
+		Parse(a_GLTFPath);
 	}
 
 	GLTFParser::~GLTFParser() {}
@@ -15,12 +15,14 @@ namespace BBE {
 	//				a path to a single file because it can contain
 	//				both a GLTF file and a .bin file.
 
-	GLTFFile* GLTFParser::Parse(std::string a_GLTFPath, std::string a_BinFile) {
+	GLTFFile* GLTFParser::Parse(std::string a_GLTFPath) {
 		
 		GLTFFile* file = reinterpret_cast<GLTFFile*>(malloc(sizeof(GLTFFile)));
 
 		JsonParser parser(a_GLTFPath.c_str());
-		m_BinFile = BBSystem::OpenFileReadBB(a_BinFile.c_str());
+		std::string binPath = "Assets/Models/Cube/glTF/";
+		binPath.append(parser.GetRootNode()["buffers"]->GetListBB()[0]->GetObjectBB()["uri"]->GetStringBB());
+		m_BinFile = BBSystem::OpenFileReadBB(binPath);
 		
 		int accessorIndex = (int)parser.GetRootNode()["meshes"]->GetListBB()[0]->GetObjectBB()["primitives"]->GetListBB()[0]->GetObjectBB()["attributes"]->GetObjectBB()["POSITION"]->GetFloatBB();
 		int bufferViewIndex = (int)parser.GetRootNode()["accessors"]->GetListBB()[accessorIndex]->GetObjectBB()["bufferView"]->GetFloatBB();
@@ -29,17 +31,20 @@ namespace BBE {
 		uint32_t byteLength = (int)parser.GetRootNode()["bufferViews"]->GetListBB()[bufferViewIndex]->GetObjectBB()["byteLength"]->GetFloatBB();
 		uint32_t byteOffset = (int)parser.GetRootNode()["bufferViews"]->GetListBB()[bufferViewIndex]->GetObjectBB()["byteOffset"]->GetFloatBB();
 		
-		unsigned char* buffer = (unsigned char*)malloc(byteLength);
-		BBSystem::ReadFileAtBB(m_BinFile, buffer, byteLength, byteOffset);
-
 		file->Vertices = reinterpret_cast<Vector3*>(malloc(bufferCount));
-		float* vertBuffer = reinterpret_cast<float*>(buffer);
+		BBSystem::ReadFileAtBB(m_BinFile, reinterpret_cast<unsigned char*>(file->Vertices), byteLength, byteOffset);
 
-		uint32_t vertIndex = 0;
-		for (size_t i = 0; i < bufferCount; i++)
-		{
-			//file->Vertices[i] = Vector3(vertBuffer[vertIndex++], vertBuffer[vertIndex++], vertBuffer[vertIndex++]);
-		}
+		// Indices
+		accessorIndex =		(int)parser.GetRootNode()["meshes"]->GetListBB()[0]->GetObjectBB()["primitives"]->GetListBB()[0]->GetObjectBB()["indices"]->GetFloatBB();
+		bufferViewIndex =	(int)parser.GetRootNode()["accessors"]->GetListBB()[accessorIndex]->GetObjectBB()["bufferView"]->GetFloatBB();
+		bufferCount =		(int)parser.GetRootNode()["accessors"]->GetListBB()[accessorIndex]->GetObjectBB()["count"]->GetFloatBB();
+
+		byteLength =		(int)parser.GetRootNode()["bufferViews"]->GetListBB()[bufferViewIndex]->GetObjectBB()["byteLength"]->GetFloatBB();
+		byteOffset =		(int)parser.GetRootNode()["bufferViews"]->GetListBB()[bufferViewIndex]->GetObjectBB()["byteOffset"]->GetFloatBB();
+
+		file->Indices = reinterpret_cast<uint32_t*>(malloc(bufferCount));
+		BBSystem::ReadFileAtBB(m_BinFile, reinterpret_cast<unsigned char*>(file->Indices), byteLength, byteOffset);
+
 
 		return file;
 	}
