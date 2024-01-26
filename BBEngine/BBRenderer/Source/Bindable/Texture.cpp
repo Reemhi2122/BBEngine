18ad5c598a68ab54b20137cb1934fd7e5f8baa63
Reemhi2122 <stanvogels@live.nl>
2024-01-26 16:22:58 +0000

    Added stb_image lib

--- BBEngine/BBRenderer/Source/Bindable/Texture.cpp
+++ BBEngine/BBRenderer/Source/Bindable/Texture.cpp
@@ -1,12 +1,6 @@
 #include "Bindable/Texture.h"
-
-#include <combaseapi.h>
-
-#include <wincodec.h>
+#include "stb_image.h"
 
 Texture::Texture(Graphics& a_Gfx)
 {
-	//CoCreateInstance();
-
-	CreateDecoderFromFilename();
 }
