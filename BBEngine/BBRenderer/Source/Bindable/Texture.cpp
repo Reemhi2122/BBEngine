0e6b684c8daf62b32c057167eec31046f5e0ece8
reemhi2122 <stanvogels@live.nl>
2024-01-27 22:01:24 +0000

    Correct path set for texture and removed redundant descriptor settings

--- BBEngine/BBRenderer/Source/Bindable/Texture.cpp
+++ BBEngine/BBRenderer/Source/Bindable/Texture.cpp
@@ -5,43 +5,44 @@
 Texture::Texture(Graphics& a_Gfx)
 {
 	int sizeX, sizeY, Channels;
-	unsigned char* img = stbi_load("D:\\PersonalProjects\\BBEngine\\BBEngine\\BBGame\\Assets\\Models\\Lantern\\glTF\\Lantern_baseColor.png", &sizeX, &sizeY, &Channels, 0);
+	unsigned char* img = stbi_load(".\\Assets\\Models\\Lantern\\glTF\\Lantern_baseColor.png", &sizeX, &sizeY, &Channels, 0);
 
-	D3D11_TEXTURE2D_DESC desc;
+	HRESULT res;
+
+	D3D11_TEXTURE2D_DESC desc = {};
 	desc.Width = sizeX;
 	desc.Height = sizeY;
-	desc.MipLevels = desc.ArraySize = 1;
+	desc.MipLevels = 1;
+	desc.ArraySize = 1;
+	desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
 	desc.SampleDesc.Count = 1;
-	desc.Usage = D3D11_USAGE_DYNAMIC;
+	desc.SampleDesc.Quality = 0;
+	desc.Usage = D3D11_USAGE_DEFAULT;
 	desc.BindFlags = D3D10_BIND_SHADER_RESOURCE;
-	desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	desc.CPUAccessFlags = 0;
 	desc.MiscFlags = 0;
 
 	D3D11_SUBRESOURCE_DATA image_subresource_data = {};
 	image_subresource_data.pSysMem = img;
-	image_subresource_data.SysMemPitch = sizeX * 4;
+	image_subresource_data.SysMemPitch = sizeX * Channels;
 
-	a_Gfx.GetDevice()->CreateTexture2D(&desc, &image_subresource_data, m_Texture.GetAddressOf());
+	res = a_Gfx.GetDevice()->CreateTexture2D(&desc, &image_subresource_data, &m_Texture);
 
-	a_Gfx.GetDevice()->CreateShaderResourceView(m_Texture.Get(), nullptr, m_ShaderResourceView.GetAddressOf());
+	D3D11_SHADER_RESOURCE_VIEW_DESC image_rsv_desc = {};
+	image_rsv_desc.Format = desc.Format;
+	image_rsv_desc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	image_rsv_desc.Texture2D.MostDetailedMip = 0;
+	image_rsv_desc.Texture2D.MipLevels = 1;
 
-	D3D11_SAMPLER_DESC image_sampler_desc;
-	image_sampler_desc.Filter = D3D11_FILTER_MIN_LINEAR_MAG_MIP_POINT;
-	image_sampler_desc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
-	image_sampler_desc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
-	image_sampler_desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
-	image_sampler_desc.MipLODBias = 0.0f;
-	image_sampler_desc.MaxAnisotropy = 1;
-	image_sampler_desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-	image_sampler_desc.BorderColor[0] = 1.0f;
-	image_sampler_desc.BorderColor[1] = 1.0f;
-	image_sampler_desc.BorderColor[2] = 1.0f;
-	image_sampler_desc.BorderColor[3] = 1.0f;
-	image_sampler_desc.MinLOD = -FLT_MAX;
-	image_sampler_desc.MaxLOD = FLT_MAX;
+	res = a_Gfx.GetDevice()->CreateShaderResourceView(m_Texture.Get(), &image_rsv_desc, &m_ShaderResourceView);
 
-	a_Gfx.GetDevice()->CreateSamplerState(&image_sampler_desc, m_SamplerState.GetAddressOf());
+	D3D11_SAMPLER_DESC image_sampler_desc = {};
+	image_sampler_desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	image_sampler_desc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	image_sampler_desc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	image_sampler_desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 
+	res = a_Gfx.GetDevice()->CreateSamplerState(&image_sampler_desc, &m_SamplerState);
 }
 
 void Texture::Bind(Graphics& a_Gfx) noexcept
