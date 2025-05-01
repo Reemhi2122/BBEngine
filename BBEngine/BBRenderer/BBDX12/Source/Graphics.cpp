7594073423e0a18e39dac4724416080aa18f10ce
Reemhi2122 <stanvogels@live.nl>
2025-05-01 23:52:12 +0100

    Getting DX12 compatible device

--- BBEngine/BBRenderer/BBDX12/Source/Graphics.cpp
+++ BBEngine/BBRenderer/BBDX12/Source/Graphics.cpp
@@ -1,6 +1,7 @@
 #include "Graphics.h"
 
 #include "combaseapi.h"
+#include <iostream>
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
@@ -16,7 +17,43 @@ bool Graphics::Initialize()
 	HRESULT hres;
 
 	IDXGIFactory4* dxgiFactory;
-	hres = CreateDXGIFactory1(IDD_PPV_ARGS(&dxgiFactory));
+	hres = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
+	if (FAILED(hres))
+	{
+		return false;
+	}
+
+	IDXGIAdapter1* dxgiAdapter;
+	uint32_t adapterIndex = 0;
+	bool adapterFound = false;
+
+	while (dxgiFactory->EnumAdapters1(adapterIndex, &dxgiAdapter) != DXGI_ERROR_NOT_FOUND)
+	{
+		DXGI_ADAPTER_DESC1 desc;
+		dxgiAdapter->GetDesc1(&desc);
+		if(!(desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE))
+		{
+			hres = D3D12CreateDevice(dxgiAdapter, D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr);
+			if (SUCCEEDED(hres))
+			{
+				adapterFound = true;
+				break;
+			}
+		}
+
+		adapterIndex++;
+	}
+
+	if (!adapterFound)
+	{
+		printf("No DX12 compatible device found!");
+		return false;
+	}
 
 	return true;
+}
+
+void Graphics::Cleanup()
+{
+
 }
