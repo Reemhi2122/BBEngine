270f48a44f0100973dffe7f02a30ef96722fded8
Reemhi2122 <stanvogels@live.nl>
2025-09-12 23:25:23 +0100

    Move drawing of stencil for directional light to component

--- BBEngine/BBEngine/Source/GameLib/Components/LightComponents/DirectionalLightComponent.cpp
+++ BBEngine/BBEngine/Source/GameLib/Components/LightComponents/DirectionalLightComponent.cpp
@@ -11,7 +11,47 @@ void DirectionalLightComponent::Update(IGraphics& a_Graphics)
 	//m_DirectionalLight->direction = m_Transform->GetPosition();
 }
 
-void DirectionalLightComponent::Draw(IGraphics& a_Graphics) {}
+void DirectionalLightComponent::Draw(IGraphics& a_Graphics) 
+{
+    float cx = -20.0f, cy = 50.0f, cz = 0.0f;
+	Vector3 FakePos = Vector3(cx, cy, cz);
+	Vector3 focusPoint = Vector3(0, 0, 0);
+
+	float length = sqrt(cx * cx + cy * cy + cz * cz);
+	m_DirectionalLight.direction = Vector3(cx / length, cy / length, cz / length);
+
+	DirectX::XMMATRIX lightView = DirectX::XMMatrixLookAtLH(
+	    DirectX::XMVectorSet(FakePos.x, FakePos.y, FakePos.z, 0),
+	    DirectX::XMVectorSet(focusPoint.x, focusPoint.y, focusPoint.z, 1.0f),
+	    DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f)
+	);
+
+	DirectX::XMMATRIX projection = DirectX::XMMatrixOrthographicLH(150.f, 150.f, 0.5f, 500.f);
+	DirectX::XMMATRIX oldProjection = m_Cam2.GetProjection();
+	m_Cam2.SetProjection(projection);
+	m_Cam2.SetViewPort(8192, 8192);
+
+	m_DirectionalLight.lightView = DirectX::XMMatrixTranspose(lightView * projection);
+
+	m_Cam2.m_ViewMatrix = lightView;
+	m_Graphics.SetCamera(&m_Cam2);
+	m_Graphics.SetDepthStencilTarget(m_DLTextureDepthStencilView);
+	
+	for (size_t i = 0; i < a_GameObjects.size(); i++)
+	{
+	    BBObject* obj = a_GameObjects[i];
+	    //obj->GetModel()->SetCurrentShader(a_VSShadowMapShader, a_PSShadowMapShader);
+	    m_Graphics.BindShader(ShaderType::VertexShader, a_VSShadowMapShader);
+	    m_Graphics.BindShader(ShaderType::PixelShader, a_PSShadowMapShader);
+	    obj->Draw(m_Graphics);
+	    //obj->GetModel()->ResetShaders();
+	}
+
+	m_Cam2.SetProjection(oldProjection);
+	m_Cam2.SetViewPort(1024, 1024);
+	m_Graphics.SetGameViewRenderTarget();
+	m_Graphics.SetCamera(&m_Cam1);
+}
 
 void DirectionalLightComponent::InspectorDrawUI()
 {
